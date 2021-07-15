128f2b966d577c61b0ba895101a4bdcf524e35a1
chinthan11shah <chinthanshah@gmail.com>
2021-07-15 12:47:41 +0530

    init

---
+++ api/views.py
@@ -0,0 +1,39 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import NoteSerializer
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            "Endpoint": "/notes/", 
+            "method": "GET", 
+            "body": None, 
+            "description": "Returns an array of notes"
+        }, 
+        {
+            "Endpoint": "/notes/id",
+            "method": "GET", 
+            "body": None, 
+            "description": "Returns a single note object"
+        }, 
+        {
+            "Endpoint": "/notes/create", 
+            "method": "POST", 
+            "body": {"body": ""}, 
+            "description": "Creates new note with data sent in post request"
+        }, 
+        {
+            "Endpoint": "/notes/id/update", 
+            "method": "PUT", 
+            "body": {"body": ""}, 
+            "description": "Creates an existing note with data sent in post request"
+        }, 
+        {
+            "Endpoint": "/notes/id/delete/", 
+            "method": "DELETE", 
+            "body": None, 
+            "description": "Deletes and existing note"
+        } 
+    ]
+    return Response(routes)
